7035baebc5de2c52e8a13f1f31830d5720236451
beka <mr.begzodjon96@mail.ru>
2020-04-14 20:18:59 +0300

    rubocop error has been fixed

--- bin/main.rb
+++ bin/main.rb
@@ -2,21 +2,19 @@ puts '                                      Hello Welcome to Tic Tac Toe        
 puts 'This game for two person. For playing game insert gamers nic! '
 
 print 'Player #1: '
-player_1 = gets.chomp
+player1 = gets.chomp
 
 print 'Player #2: '
-player_2 = gets.chomp
-puts "Great #{player_1} vs #{player_2}, Let\'s get started!"
+player2 = gets.chomp
+puts "Great #{player1} vs #{player2}, Let\'s get started!"
 puts '                                     Choose you sign in dashboard X || O '
 x = false
-print "#{player_1} make a chooce: "
+print "#{player1} make a chooce: "
 chooce = gets.chomp
 while chooce != 'X' && chooce != 'O'
-    print "#{player_1} please choose out of given option:  "
-    chooce = gets.chomp
+  print "#{player1} please choose out of given option:  "
+  chooce = gets.chomp
 end
 chooce == 'X' ? x = true : nil
-puts x ? "#{player_2} will play with sign O vs #{player_1} with sign X" : "#{player_2} will play with sign X vs #{player_1} with sign O"
 
-
-
+puts x ? "#{player2} - O vs #{player1} - X" : "#{player2} - X vs #{player1} - O"
