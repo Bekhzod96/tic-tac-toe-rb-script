d22ec4035752c35c91cedba8786f7f22209e795b
beka <mr.begzodjon96@mail.ru>
2020-04-16 19:44:32 +0300

    - Class game and Player implemented

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -2,65 +2,60 @@
 
 class Player
   attr_reader :name, :sign
+  attr_writer :sign
 
-  def initialize(name, sign)
+  def initialize(name)
     @name = name
-    @sign = sign
-  end
-end
-
-class Board
-  def initialize
-    @board = {
-      1 => "1", 2 => "2", 3 => "3",
-      4 => "4", 5 => "5", 6 => "6",
-      7 => "7", 8 => "8", 9 => "9",
-    }
-  end
-
-  def display_board
-    " Result of the move
-    #{@board[1]} | #{@board[2]} | #{@board[3]} \n
-    #{@board[4]} | #{@board[5]} | #{@board[6]} \n
-    #{@board[7]} | #{@board[8]} | #{@board[9]} "
+    @sign = ''
   end
 end
 
 class Game
-  def initialize(player1_name, player1_sign, player2_name, player2_sign)
-    @player1_name = player1_name
-    @player1_sign = player1_sign
-    @player2_name = player2_name
-    @player2_sign = player2_sign
+  attr_accessor :board, :p1, :p2, :sign
+  def initialize(player1_name, player2_name,sign)
+    @board = { 1 => '1', 2 => '2', 3 => '3', 4 => '4', 5 => '5', 6 => '6', 7 => '7', 8 => '8', 9 => '9' }
+    @p1 = Player.new(player1_name)
+    @p2 = Player.new(player2_name)
+    @track = true
+    add_player_sign(sign)
   end
 
-  @board = Board.new
-  @p1 = Player.new(@player1_name, @player1_sign)
-  @p2 = Player.new(@player2_name, @player2_sign)
-
-  def looping_game?
-    player1_turn = true
-    while check_win(@board.board).empty? && draw(@board.board)
-      if player1_turn
-        board = move(@p1.name, board, @p1.sign)
-        player1_turn = false
-      else
-        board = move(@p2.name, board, @p2.sign)
-        player1_turn = true
-      end
+  def add_player_sign(sign)
+    if sign == 'X'
+      @p1.sign = 'X'
+      @p2.sign = 'O'
+    else
+      @p1.sign = 'O'
+      @p2.sign = 'X'
     end
-    check_win(board)
   end
 
-  def check_win(board)
+  def display
+    "  \nResult of the move\n
+    #{@board[1]} | #{@board[2]} | #{@board[3]} \n
+    #{@board[4]} | #{@board[5]} | #{@board[6]} \n
+    #{@board[7]} | #{@board[8]} | #{@board[9]} "
+  end
+
+  def update_board(move)
+    if @track
+      @board = move.call(@p1.name, @board, @p1.sign)
+      @track = false
+    else
+      @board = move.call(@p2.name, @board, @p2.sign)
+      @track = true
+    end
+  end
+
+  def check_win?
     result = []
     winnig = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
     winnig.each do |arr|
-      sign = ""
+      sign = ''
       arr.each_with_index do |value, index|
-        if (board[value] == "X" || board[value] == "O") && index.zero?
-          sign = board[value]
-        elsif sign == board[value]
+        if (@board[value] == 'X' || @board[value] == 'O') && index.zero?
+          sign = @board[value]
+        elsif sign == @board[value]
           return result = [true, sign] if index == 2
 
           next
@@ -72,25 +67,12 @@ class Game
     result
   end
 
-  def draw(board)
+  def draw?
     draw = true
     board.each_with_index do |value, index|
       break draw = false if index == 8
-      value[1] == "X" || value[1] == "O" ? next : break
+      value[1] == 'X' || value[1] == 'O' ? next : break
     end
     draw
-  end
-
-  def add_player_sign(player_sign, player1, player2)
-    x = false
-    player_sign == "X" ? x = true : nil
-    sign = {}
-    if x
-      sign[player1] = "X"
-      sign[player2] = "O"
-    else
-      sign[player1] = "O"
-      sign[player2] = "X"
-    end
   end
 end
